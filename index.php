e723bc0b2903d71ff00883783f88a99f3834de96
aslam45 <achoudhury@live.co.uk>
2025-09-19 10:47:09 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!-- <!DOCTYPE html>
+<html  <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <?php wp_head(); ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+ 
+</head>
+<body <? body_class('example'); ?>>
+  <?php wp_body_open(); ?>
+  <p>Hello World!</p>
+
+  <?php wp_footer(); ?>
+</body>
+</html> -->
+
+<!-- This file is for learning purposes therefore, should always be empty -->
